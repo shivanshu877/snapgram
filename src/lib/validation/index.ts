949da59af52c3d67499a3ccfa915cd58c92d821b
shivanshu877 <vashudev8777@gmail.com>
2024-04-01 16:50:19 +0530

    auth completed

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -8,3 +8,10 @@ export const SignupValidation = z.object({
     .string()
     .min(6, { message: "Must be at least 6 characters long" }),
 });
+
+export const SigninValidation = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(6, { message: "Must be at least 6 characters long" }),
+});
